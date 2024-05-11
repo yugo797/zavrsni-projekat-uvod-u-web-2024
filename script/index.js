fa88d65129b105db9f36f13e313c7016ce69eadc
Yu <milica.m.simovic3@gmail.com>
2024-05-12 01:02:10 +0200

    added search function

--- script/index.js
+++ script/index.js
@@ -1,6 +1,8 @@
 "use strict";
 const loadmoreBtn = document.getElementsByClassName("loadMoreProducts")[0];
 const gameProduct = document.querySelector(".products");
+const srcBtn = document.querySelector(".srcBtn");
+const inputTxt = document.querySelector(".inputSearch");
 
 let i = 0;
 function generateCard(id, thumbnail, name, price) {
@@ -13,7 +15,7 @@ function generateCard(id, thumbnail, name, price) {
         <div class="game-data">
             <h3>${name}</h3>
             <span>${price}$</span>
-            <button>add to cart</button>
+            <button>Read More</button>
         </div>
         </div>`;
 }
@@ -34,5 +36,27 @@ function generateProduct() {
     });
 }
 
+function searchFunct(srcQuery) {
+  console.log(`Searching for ${srcQuery}`);
+  gameProduct.innerHTML = "";
+  fetch(`https://www.cheapshark.com/api/1.0/games?title=${srcQuery}`)
+    .then((rez) => rez.json())
+    .then((data) => {
+      for (let j = 0; j < data.length; j++) {
+        gameProduct.innerHTML += generateCard(
+          data[j].internalName,
+          data[j].thumb,
+          data[j].external,
+          data[j].cheapest
+        );
+      }
+    });
+}
+
 window.onload = generateProduct;
 loadmoreBtn.addEventListener("click", generateProduct);
+
+srcBtn.addEventListener("click", function () {
+  const searchValue = inputTxt.value;
+  searchFunct(searchValue);
+});
