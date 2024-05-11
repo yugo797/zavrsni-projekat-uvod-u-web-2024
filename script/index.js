3a29aac03b22593573912c308f029f6ff089bbc6
Yu <milica.m.simovic3@gmail.com>
2024-05-11 23:49:31 +0200

    added load more button and query

--- script/index.js
+++ script/index.js
@@ -1,31 +1,38 @@
 "use strict";
-
+const loadmoreBtn = document.getElementsByClassName("loadMoreProducts")[0];
 const gameProduct = document.querySelector(".products");
 
-function generateProduct(id, thumbnail, name, price) {
+let i = 0;
+function generateCard(id, thumbnail, name, price) {
   return `
-    <div class="single-product">
-    <img
-      src="${thumbnail}"
-      alt="${id}"
-    />
-    <div class="game-data">
-      <h3>${name}</h3>
-      <span>${price}</span>
-      <button>add to cart</button>
-    </div>
-  </div>`;
+        <div class="single-product">
+        <img
+        src="${thumbnail}"
+        alt="${id}"
+        />
+        <div class="game-data">
+            <h3>${name}</h3>
+            <span>${price}$</span>
+            <button>add to cart</button>
+        </div>
+        </div>`;
 }
 
-fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice=50")
-  .then((rez) => rez.json())
-  .then((data) => {
-    for (let i = 0; i < 13; i++) {
-      gameProduct.innerHTML += generateProduct(
-        data[i].internalName,
-        data[i].thumb,
-        data[i].external,
-        data[i].cheapest
-      );
-    }
-  });
+function generateProduct() {
+  fetch("https://www.cheapshark.com/api/1.0/deals?storeID=1&upperPrice>=0")
+    .then((rez) => rez.json())
+    .then((data) => {
+      for (let j = i; j < i + 3; j++) {
+        gameProduct.innerHTML += generateCard(
+          data[j].internalName,
+          data[j].thumb,
+          data[j].title,
+          data[j].normalPrice
+        );
+      }
+      i += 3;
+    });
+}
+
+window.onload = generateProduct;
+loadmoreBtn.addEventListener("click", generateProduct);
