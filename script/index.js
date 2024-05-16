120fd8f4253c0d9c10950b5a118f3df2343b0c7e
Yu <milica.m.simovic3@gmail.com>
2024-05-16 19:19:47 +0200

    added text to readmore

--- script/index.js
+++ script/index.js
@@ -78,6 +78,10 @@ function generateModalCard(
           <div class="modal-card-info">
             <h3>${internalName}</h3>
             <p>Steam App ID: ${steamAppID || "N/A"}</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
             <p>Cheapest Deal: ${cheapest}$</p>
             <a href="${external}" target="_blank">View on Store</a>
           </div>
